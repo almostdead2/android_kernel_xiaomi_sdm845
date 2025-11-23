524950774465898f5d71b34114289b659228ed24
almostdead2 <ajoygandu79@gmail.com>
2025-11-23 10:37:37 +0530

    For OPSLA

--- include/net/rtnetlink.h
+++ include/net/rtnetlink.h
@@ -18,7 +18,7 @@ int rtnl_register_module(struct module *owner, int protocol, int msgtype,
 int __rtnl_register(int protocol, int msgtype,
 		    rtnl_doit_func, rtnl_dumpit_func, rtnl_calcit_func, unsigned int flags);
 void rtnl_register(int protocol, int msgtype,
-		   rtnl_doit_func, rtnl_dumpit_func, rtnl_calcit_func, unsigned int flags);
+		   rtnl_doit_func, rtnl_dumpit_func, unsigned int flags);
 int rtnl_unregister(int protocol, int msgtype);
 void rtnl_unregister_all(int protocol);
 
