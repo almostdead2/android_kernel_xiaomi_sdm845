fb1d9c672aad38c4a21905a3ede716f2c52fc362
almostdead2 <ajoygandu79@gmail.com>
2025-11-27 15:41:29 +0530

    For OPSLA

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -125,6 +125,11 @@ int sysctl_tcp_invalid_ratelimit __read_mostly = HZ/2;
 #define TCP_REMNANT (TCP_FLAG_FIN|TCP_FLAG_URG|TCP_FLAG_SYN|TCP_FLAG_PSH)
 #define TCP_HP_BITS (~(TCP_RESERVED_BITS|TCP_FLAG_PSH))
 
+/* WIFI MODIFICATION */
+void (*statistic_dev_rtt)(struct sock *sk, long rtt) = NULL;
+EXPORT_SYMBOL(statistic_dev_rtt);
+/* WIFI MODIFICATION */
+
 #define REXMIT_NONE	0 /* no loss recovery to do */
 #define REXMIT_LOST	1 /* retransmit packets marked lost */
 #define REXMIT_NEW	2 /* FRTO-style transmit of unsent/new packets */
@@ -773,6 +778,11 @@ static void tcp_rtt_estimator(struct sock *sk, long mrtt_us)
 			tp->rtt_seq = tp->snd_nxt;
 			tp->mdev_max_us = tcp_rto_min_us(sk);
 		}
+		/* WIFI MODIFICATION */
+		if (sk->sk_state == TCP_ESTABLISHED && statistic_dev_rtt) {
+			statistic_dev_rtt(sk, mrtt_us);
+		}
+		/* WIFI MODIFICATION */
 	} else {
 		/* no previous measure. */
 		srtt = m << 3;		/* take the measured time to be rtt */
