2bfa779e9b95991160ad056957c1cbfb60262011
almostdead2 <ajoygandu79@gmail.com>
2025-12-10 08:21:07 +0530

    Modified AG For SDM845

--- drivers/oneplus/coretech/aigov/aigov.c
+++ drivers/oneplus/coretech/aigov/aigov.c
@@ -35,33 +35,29 @@ module_param_named(use_util, _aigov_use_util, bool, 0664);
 static bool aigov_can_update = false;
 module_param_named(aigov_can_update, aigov_can_update, bool, 0664);
 
-/* global resource */
-static int g_cpufreq[3] = {0};
+/* global resource - SDM845: 2 clusters */
+static int g_cpufreq[2] = {0};
 static int g_ddrfreq = 0;
-static atomic_t g_boost_hint[3] = {ATOMIC_INIT(0)};
+static atomic_t g_boost_hint[2] = {ATOMIC_INIT(0)};
 
 /* aigov weight */
 static unsigned int aigov_weight = 50;
 
 DEFINE_PER_CPU(struct aigov_stat, aig_stat);
 
-/* helper */
+/* helper - SDM845: 4+4 topology */
 static inline int cpu_to_clus(int cpu)
 {
 	if (cpu < 4)
 		return 0;
-	if (cpu < 7)
-		return 1;
-	return 2;
+	return 1;
 }
 
 static inline int clus_to_first_cpu(int clus)
 {
 	if (clus == 0)
 		return 0;
-	if (clus == 1)
-		return 4;
-	return 7;
+	return 4;
 }
 
 bool aigov_enabled(void)
@@ -88,7 +84,7 @@ void aigov_reset(void)
 		return;
 	}
 
-	for (i = 0; i < 3; ++i) {
+	for (i = 0; i < 2; ++i) {
 		struct aigov_stat* ais = &per_cpu(aig_stat, clus_to_first_cpu(i));
 		*ais->pred_cpufreq = 0;
 		*ais->pred_ddrfreq = 0;
@@ -166,14 +162,12 @@ int aigov_get_boost_hint(int cpu)
 
 void aigov_dump(int cpu, unsigned long util, unsigned long aig_util, unsigned long extra_util)
 {
-	aigov_logv("cpu%d %d cpu%d %d cpu%d %d ddrfreq %d boost %d %d %d\n",
+	aigov_logv("cpu%d %d cpu%d %d ddrfreq %d boost %d %d\n",
 		0, aigov_get_cpufreq(0),
 		4, aigov_get_cpufreq(4),
-		7, aigov_get_cpufreq(7),
 		aigov_get_ddrfreq(),
 		aigov_get_boost_hint(0),
-		aigov_get_boost_hint(4),
-		aigov_get_boost_hint(7));
+		aigov_get_boost_hint(4));
 
 	aigov_logv("cpu%d: orig: %llu, aig: %llu: extra: %llu\n",
 		cpu, util, aig_util, extra_util);
@@ -254,12 +248,11 @@ static struct kernel_param_ops aigov_dump_ops = {
 };
 module_param_cb(aigov_dump, &aigov_dump_ops, NULL, 0444);
 
-/* test trigger entry */
+/* test trigger entry - SDM845: only 2 clusters */
 static int aigov_trigger_show(char *buf, const struct kernel_param *kp)
 {
 	aigov_update_util(0);
 	aigov_update_util(1);
-	aigov_update_util(2);
 
 	return snprintf(buf, PAGE_SIZE, "triggered\n");
 }
